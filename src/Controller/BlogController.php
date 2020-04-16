1154d52116173ce84ac8b028cf51cc6904c951ee
Thomas Dem <demagny.t@hotmail.fr>
2020-04-16 14:25:04 +0200

    added BlogController

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -2,9 +2,14 @@
 
 namespace App\Controller;
 
+use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Article;
 
 /**
  * Class BlogController
@@ -14,12 +19,18 @@ class BlogController extends AbstractController
 {
     /**
      * @Route("/blog", name="blog")
+     * @param ArticleRepository $repository
+     * @return Response
      */
-    public function index()
+    public function index(ArticleRepository $repository)//injection de dépendance
     {
-        return $this->render('blog/index.html.twig', [
-            'controller_name' => 'BlogController',
-        ]);
+
+        $articles = $repository->findAll();
+        return $this->render('blog/index.html.twig',
+            [
+                'controller_name' => 'BlogController',
+                'articles'=>$articles
+            ]);
     }
 
     /**
@@ -28,14 +39,57 @@ class BlogController extends AbstractController
      */
     public function home(){
 
-    return $this->render('blog/home.html.twig');
-}
+        return $this->render('blog/home.html.twig');
+    }
 
     /**
-     * @Route ("/blog/12", name="blog_show")
+     * @Route("/blog/new", name="blog_create")
      * @return Response
      */
-    public function show(){
-        return $this->render('blog/show.html.twig');
-}
+    public function create(){
+
+        $article = new Article();
+
+        $form = $this->createFormBuilder($article)
+            ->add('title', TextType::class,[
+                'attr' => [
+                    'placeholder' => "Titre de l'article",
+                    "class" => 'form-control'
+                ]
+            ])
+            ->add('content',TextareaType::class, [
+                'attr' => [
+                    "placeholder" => "Contenu de l'article",
+                    "class" => 'form-control'
+                ]
+            ])
+            ->add('image' ,TextType::class, [
+                'attr' => [
+                    "placeholder" => "Image de l'article",
+                    "class" => 'form-control'
+                ]
+        ])
+            ->add('save', SubmitType::class)
+            ->getForm();
+
+        return $this->render('blog/create.html.twig', [
+            'formArticle' => $form->createView()
+        ]);
+    }
+
+
+    /**
+     * @Route ("/blog/{id}", name="blog_show")
+     * @param Article $article
+     * @return Response
+     */
+    public function show(Article $article){
+        /*remplace =>(ArticleRepository $repository, $id)  $repository = $this->getDoctrine()->getRepository(Article::class);
+         et $article = $repository->find($id); GRACE AU PARAMCONVERTER*/
+
+        return $this->render('blog/show.html.twig',[
+            'article'=>$article
+        ]);
+    }
+
 }
